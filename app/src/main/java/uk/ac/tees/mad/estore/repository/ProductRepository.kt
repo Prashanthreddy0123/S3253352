619c5daa330f932f81db2bb54a6d0e54ad58e09b
Prashanthreddy0123 <prashanthprashu0123@gmail.com>
2024-12-23 11:19:21 +0000

    commit 5

---
+++ app/src/main/java/uk/ac/tees/mad/estore/repository/ProductRepository.kt
@@ -0,0 +1,9 @@
+package uk.ac.tees.mad.estore.repository
+
+import uk.ac.tees.mad.estore.domain.Product
+
+interface ProductRepository {
+    suspend fun getAllProducts(): Result<List<Product>>
+    suspend fun getProductsByCategory(category: String): Result<List<Product>>
+    suspend fun getCategories(): Result<List<String>>
+}
---
+++ app/src/main/java/uk/ac/tees/mad/estore/repository/ProductRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package uk.ac.tees.mad.estore.repository
+
+import uk.ac.tees.mad.estore.data.ApiService
+import uk.ac.tees.mad.estore.domain.Product
+import javax.inject.Inject
+
+class ProductRepositoryImpl @Inject constructor(
+    private val apiService: ApiService
+) : ProductRepository {
+    override suspend fun getAllProducts(): Result<List<Product>> =
+        try {
+            Result.success(apiService.getAllProducts())
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+
+    override suspend fun getProductsByCategory(category: String): Result<List<Product>> =
+        try {
+            Result.success(apiService.getProductsByCategory(category))
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+
+    override suspend fun getCategories(): Result<List<String>> =
+        try {
+            Result.success(apiService.getCategories())
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+}
